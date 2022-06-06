eefb32648f4e7bafd36edda5a084946cca3d180e
jsscheller <jsscheller@users.noreply.github.com>
2022-06-06 15:29:28 -0700

    publish.sh

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eo pipefail
+
+cp package.json dist/
+cp README.md dist/
+cp LICENSE dist/
+
+cd dist
+
+if [[ "$LIVE" != "1" ]]
+then
+  EXTRA_FLAGS="--dry-run"
+fi
+
+npm publish \
+  --access public \
+  $EXTRA_FLAGS
